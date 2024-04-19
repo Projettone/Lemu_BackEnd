6647f94db4bb3c8b3e364ff1fc1f3946aced9e0b
Francesco-Campagna <francesco.campa2010@libero.it>
2024-04-19 11:03:09 +0200

    first commit

---
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/LemuBackEndApplication.java
@@ -0,0 +1,13 @@
+package it.unical.ea.lemubackend.lemu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemuBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LemuBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unical/ea/lemubackend/lemu_backend/LemuBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.ea.lemubackend.lemu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemuBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
