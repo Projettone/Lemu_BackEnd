5e15734be6a1037a74fc5752e61072843fa1c033
hackpietro <pietrocimino3@gmail.com>
2024-05-16 15:57:17 +0200

    backend: Modifiche alla wishlist
    * Implementazione WishlistController
    * Implementazione base WishlistDao
    * Implementazione entity Carrello
    * Implementazione entity Wishlist
    * Implementazione WishlistService
    * Implementazione WishlistServiceImpl
    * Implementazione WishlistDto

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/WishlistController.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/WishlistController.java
@@ -1,4 +1,41 @@
 package it.unical.ea.lemubackend.lemu_backend.controller;
 
+import it.unical.ea.lemubackend.lemu_backend.dto.WishlistDto;
+import it.unical.ea.lemubackend.lemu_backend.data.service.WishlistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/wishlist")
 public class WishlistController {
+    @Autowired
+    private WishlistService wishlistService;
+
+    @PostMapping
+    public ResponseEntity<WishlistDto> createWishlist(@RequestBody WishlistDto wishlistDto) {
+        WishlistDto createdWishlist = wishlistService.saveWishlist(wishlistDto);
+        return ResponseEntity.ok(createdWishlist);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WishlistDto> getWishlistById(@PathVariable Long id) {
+        Optional<WishlistDto> wishlistDto = wishlistService.getWishlistById(id);
+        return wishlistDto.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/utente/{utenteId}")
+    public ResponseEntity<WishlistDto> getWishlistByUtenteId(@PathVariable Long utenteId) {
+        WishlistDto wishlistDto = wishlistService.getWishlistByUtenteId(utenteId);
+        return ResponseEntity.ok(wishlistDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWishlist(@PathVariable Long id) {
+        wishlistService.deleteWishlist(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/WishlistService.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/WishlistService.java
@@ -1,4 +1,12 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
+import it.unical.ea.lemubackend.lemu_backend.dto.WishlistDto;
+
+import java.util.Optional;
+
 public interface WishlistService {
+    WishlistDto saveWishlist(WishlistDto wishlistDto);
+    Optional<WishlistDto> getWishlistById(Long id);
+    WishlistDto getWishlistByUtenteId(Long utenteId);
+    void deleteWishlist(Long id);
 }
