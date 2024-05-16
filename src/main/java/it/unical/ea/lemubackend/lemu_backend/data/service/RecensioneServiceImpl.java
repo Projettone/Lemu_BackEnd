da308deb5858e5a23b128a314a853c174fef28c0
Francesco-Campagna <francesco.campa2010@libero.it>
2024-05-16 19:45:13 +0200

    BackEnd: RecensioneService, RecensioneServiceImpl
    * RecensioneService: aggiunta metodi
    * RecensioneServiceImpl: aggiunta metodi

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/RecensioneServiceImpl.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/RecensioneServiceImpl.java
@@ -1,16 +1,45 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
+import it.unical.ea.lemubackend.lemu_backend.data.dao.RecensioneDao;
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Recensione;
+import it.unical.ea.lemubackend.lemu_backend.dto.RecensioneDto;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RecensioneServiceImpl implements RecensioneService{
 
+    private final RecensioneDao recensioneDao;
+    private final ModelMapper modelMapper;
+
 
     @Override
     public void save(Recensione recensione) {
 
+    }
+
+
+    @Override
+    public RecensioneDto getById(Long id) {
+        Recensione recensione = recensioneDao.findById(id)
+                .orElseThrow(
+                        () -> new EntityNotFoundException(String.format("Nessuna recensione corrispondente a id: [%s]", id)));
+        return modelMapper.map(recensione, RecensioneDto.class);
+
+    }
+
+    @Override
+    public Collection<RecensioneDto> findAll() {
+        return recensioneDao.findAll().stream()
+                .map(recensione -> modelMapper.map(recensione, RecensioneDto.class))
+                .collect(Collectors.toList());
     }
 }
