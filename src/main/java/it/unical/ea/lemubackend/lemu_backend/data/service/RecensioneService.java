cbc497d8b66ca1daa8ad41131809d17fc0751eda
Francesco-Campagna <francesco.campa2010@libero.it>
2024-05-16 19:45:13 +0200

    BackEnd: RecensioneService, RecensioneServiceImpl
    * RecensioneService: aggiunta metodi
    * RecensioneServiceImpl: aggiunta metodi

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/RecensioneService.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/RecensioneService.java
@@ -1,8 +1,16 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Recensione;
+import it.unical.ea.lemubackend.lemu_backend.dto.RecensioneDto;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface RecensioneService {
 
     void save(Recensione recensione);
+    
+    Collection<RecensioneDto> findAll();
+
+    RecensioneDto getById(Long id);
 }
