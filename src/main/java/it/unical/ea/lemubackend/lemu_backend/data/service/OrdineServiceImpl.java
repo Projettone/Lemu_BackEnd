c87dc0518307cee7340d81bb76af9efa8baa32c4
Cataldo Federico <fedex46@outlook.it>
2024-05-19 13:49:09 +0200

    backEnd: aggiunto in OrdineServiceImpl l'override dei metodi save, getById, findAll

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/OrdineServiceImpl.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/OrdineServiceImpl.java
@@ -1,4 +1,33 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
-public class OrdineServiceImpl {
+import it.unical.ea.lemubackend.lemu_backend.data.dao.OrdineDao;
+import it.unical.ea.lemubackend.lemu_backend.data.dao.UtenteDao;
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Ordine;
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Prodotto;
+import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+@Service
+@RequiredArgsConstructor
+public class OrdineServiceImpl implements OrdineService {
+
+    private final OrdineDao ordineDao;
+    private final OrdineDao articoloDao;
+    private final UtenteDao utenteDao;
+    @Override
+    public void save(Ordine ordine) {
+        ordineDao.save(ordine);
+    }
+
+    @Override
+    public ProdottoDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<ProdottoDto> findAll() {
+        return null;
+    }
 }
