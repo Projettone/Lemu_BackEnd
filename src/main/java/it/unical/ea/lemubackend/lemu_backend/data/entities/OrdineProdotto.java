a3e605aac9099455883a0ff6e4986375d7697234
Cataldo Federico <fedex46@outlook.it>
2024-05-29 15:03:51 +0200

    backend: modifica aggiunta a entities Ordine

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/entities/OrdineProdotto.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/entities/OrdineProdotto.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Entity
 @Data
 @Table(name = "ordineProdotto")
@@ -14,15 +17,22 @@ public class OrdineProdotto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idOrdine")
-    private Ordine ordine;
+    @Column(name = "indirizzo")
+    private Indirizzo indirizzo;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "idProdotto")
-    private Prodotto prodotto;
+    @JoinColumn(name = "acquirente")
+    private Utente utente;
 
-    @Column(name = "quantità")
-    private int quantita;
+
+    @Column(name = "data_acquisto")
+    private LocalDate dataAcquisto;
+
+    @Column(name = "prezzoTotaleOrdine")
+    private double prezzoTotaleOrdine;
+
+    @OneToMany(mappedBy = "ordine", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrdineProdotto> ordineProdotti; 
+
 
 }
