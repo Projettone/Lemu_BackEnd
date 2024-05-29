ead555eac24080007bf03dbe21d2ccee9a102bda
Cataldo Federico <fedex46@outlook.it>
2024-05-29 14:44:21 +0200

    backend: aggiunta entities OrdineProdotto

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/entities/OrdineProdotto.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/entities/OrdineProdotto.java
@@ -1,2 +1,28 @@
-package it.unical.ea.lemubackend.lemu_backend.data.entities;public class OrdineProdotto {
+package it.unical.ea.lemubackend.lemu_backend.data.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "ordineProdotto")
+@NoArgsConstructor
+public class OrdineProdotto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idOrdine")
+    private Ordine ordine;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "idProdotto")
+    private Prodotto prodotto;
+
+    @Column(name = "quantità")
+    private int quantita;
+
 }
