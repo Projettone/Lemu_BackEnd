119f87cc95f3d91cd0a483a31e19b64e42eafbca
hackpietro <pietrocimino3@gmail.com>
2024-05-29 15:31:59 +0200

    backend: aggiunto @Repository

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/CarrelloDao.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/CarrelloDao.java
@@ -3,6 +3,8 @@ package it.unical.ea.lemubackend.lemu_backend.data.dao;
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Carrello;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+@Repository
 public interface CarrelloDao extends JpaRepository<Carrello, Long> {
 
 }
