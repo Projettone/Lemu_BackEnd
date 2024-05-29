4b63c353ffe15eb2bfece979f97422e1b0ba4c3d
Cataldo Federico <fedex46@outlook.it>
2024-05-29 14:26:20 +0200

    backend: aggiunto @Repository

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/OrdineDao.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/OrdineDao.java
@@ -2,7 +2,9 @@ package it.unical.ea.lemubackend.lemu_backend.data.dao;
 
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Ordine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrdineDao extends JpaRepository<Ordine, Long> {
 
 }
