5464b0cb5c9fc0643364f53ea1479137d04d907b
Cataldo Federico <fedex46@outlook.it>
2024-05-29 14:23:30 +0200

    backend: Aggiunta estensione jpaRepository

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/ProdottoDao.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/ProdottoDao.java
@@ -2,6 +2,8 @@ package it.unical.ea.lemubackend.lemu_backend.data.dao;
 
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Prodotto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProdottoDao extends JpaRepository<Prodotto, Long> {
 }
---
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/entities/OrdineProdotto.java
@@ -0,0 +1,2 @@
+package it.unical.ea.lemubackend.lemu_backend.data.entities;public class OrdineProdotto {
+}
