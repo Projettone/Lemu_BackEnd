08a3ed69c3a7e8b9c3d5c4a2a115e3e750878664
Cataldo Federico <fedex46@outlook.it>
2024-06-20 10:51:48 +0200

    backend: creata classe 'ProdottoDto'

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/dto/ProdottoDto.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/dto/ProdottoDto.java
@@ -1,6 +1,42 @@
 package it.unical.ea.lemubackend.lemu_backend.dto;
 
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Recensione;
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Utente;
 import it.unical.ea.lemubackend.lemu_backend.data.service.ProdottoService;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-public class ProdottoDto implements ProdottoService {
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@ToString
+public class ProdottoDto{
+
+    private Long id;
+
+    private String nome;
+
+    private String descrizione;
+
+    private Float prezzo;
+
+    private int venduti;
+
+    private String categoria;
+
+    private String immagineProdotto;
+
+    private double valutazione;
+
+    private int numeroRecensioni;
+
+    private List<Recensione> recensione;
+
+    private Utente utente;
+
+    private int disponibilita;
+
+
 }
