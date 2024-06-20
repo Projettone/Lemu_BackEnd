a882a72fd080d5e5132f6738115f7eb4ba786589
Cataldo Federico <fedex46@outlook.it>
2024-06-20 10:44:26 +0200

    backend: aggiunti controller in 'ProdottoController'

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/ProdottoController.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/ProdottoController.java
@@ -1,4 +1,40 @@
 package it.unical.ea.lemubackend.lemu_backend.controller;
 
+import it.unical.ea.lemubackend.lemu_backend.data.service.ProdottoService;
+import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/prodotti")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdottoController {
+
+    private final ProdottoService prodottoService;
+
+
+
+    @PostMapping("/prodotti")
+    public ResponseEntity<ProdottoDto> add(@RequestBody @Valid ProdottoDto prodotto, @RequestParam String jwt) {
+        return ResponseEntity.ok(prodottoService.save(prodotto, jwt));
+    }
+
+    @GetMapping("/prodotti")
+    public ResponseEntity<Collection<ProdottoDto>> findAll() {
+        return ResponseEntity.ok(prodottoService.findAll());
+    }
+
+    @GetMapping("/prodotti/{idItem}")
+    public ResponseEntity<ProdottoDto> getById(@PathVariable("idItem") Long id) {
+        return ResponseEntity.ok(prodottoService.getById(id));
+    }
+
+
+
+
 }
