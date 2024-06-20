6f57846dcc64fabca64a3f25514bb4da71c1f128
Cataldo Federico <fedex46@outlook.it>
2024-06-20 10:50:11 +0200

    backend: aggiunto metodo in 'OrdineDao'

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/OrdineDao.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/OrdineDao.java
@@ -1,11 +1,15 @@
 package it.unical.ea.lemubackend.lemu_backend.data.dao;
 
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Ordine;
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrdineDao extends JpaRepository<Ordine, Long> {
+    List<Ordine> findByUtente(Utente utente);
 
 }
