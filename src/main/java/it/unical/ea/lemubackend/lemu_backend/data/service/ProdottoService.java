a574aba253e599d86f1e69a96efa639129349218
Cataldo Federico <fedex46@outlook.it>
2024-06-20 10:48:37 +0200

    backend: aggiunti metodi in 'ProdottoService'

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/ProdottoService.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/ProdottoService.java
@@ -1,4 +1,12 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
+import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
+
+import java.util.Collection;
+
 public interface ProdottoService {
+
+    ProdottoDto save(ProdottoDto prodottoDto, String jwt);
+    Collection<ProdottoDto> findAll();
+    ProdottoDto getById(Long id);
 }
