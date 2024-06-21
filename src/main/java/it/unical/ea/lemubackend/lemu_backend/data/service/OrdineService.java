6667c7d720aff83c852f9fc32961aae79dbc8cbb
Cataldo Federico <fedex46@outlook.it>
2024-06-21 19:26:39 +0200

    backend: aggiornamento metodi 'OrdineService'

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/OrdineService.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/OrdineService.java
@@ -1,18 +1,12 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
-import it.unical.ea.lemubackend.lemu_backend.data.entities.Ordine;
-import it.unical.ea.lemubackend.lemu_backend.data.entities.Prodotto;
 import it.unical.ea.lemubackend.lemu_backend.dto.OrdineDto;
-import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.http.HttpStatus;
 
 import java.util.Collection;
-import java.util.concurrent.CompletableFuture;
 
 public interface OrdineService {
 
-    HttpStatus save(OrdineDto ordine, String jwt);
+    boolean save(OrdineDto ordine, String jwt);
     OrdineDto getById(Long id);
     Collection<OrdineDto> findOrderbyUser(String jwt);
 
