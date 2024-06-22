c9c6021d06f419df1006405ef0be225b2f7bcd55
Cataldo Federico <fedex46@outlook.it>
2024-06-22 10:27:21 +0200

    backend: aggiornamento metodi in ProdottoService

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/ProdottoService.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/service/ProdottoService.java
@@ -1,12 +1,14 @@
 package it.unical.ea.lemubackend.lemu_backend.data.service;
 
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Prodotto;
 import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
 
 import java.util.Collection;
 
 public interface ProdottoService {
 
-    boolean save(ProdottoDto prodottoDto, String jwt);
+    ProdottoDto save(ProdottoDto prodottoDto, String jwt);
     Collection<ProdottoDto> findAll();
     ProdottoDto getById(Long id);
+    void save(Prodotto prodotto);
 }
