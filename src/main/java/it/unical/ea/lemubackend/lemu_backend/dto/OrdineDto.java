0dcee14a9d0e08ecb35988862a73d6aac881a684
Cataldo Federico <fedex46@outlook.it>
2024-08-18 18:47:02 +0200

    backend: modifiche in OrdineDto

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/dto/OrdineDto.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/dto/OrdineDto.java
@@ -1,3 +1,27 @@
 package it.unical.ea.lemubackend.lemu_backend.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@ToString
 public class OrdineDto {
+
+    private Long id;
+
+    private String indirizzo;
+
+    private Long idutente;
+
+    private LocalDate dataAcquisto;
+
+    private Double prezzoTotaleOrdine;
+
+    private List<OrdineProdottoDto> ordineProdotti;
+
 }
