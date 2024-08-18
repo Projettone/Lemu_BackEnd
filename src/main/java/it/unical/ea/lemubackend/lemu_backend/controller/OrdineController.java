71a4f64da89cc9a2652aa91c71bf2dedb7eb262a
Cataldo Federico <fedex46@outlook.it>
2024-08-18 18:40:31 +0200

    backend: modifiche ordineController

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/OrdineController.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/OrdineController.java
@@ -1,16 +1,17 @@
 package it.unical.ea.lemubackend.lemu_backend.controller;
 
-import it.unical.ea.lemubackend.lemu_backend.config.ApiResponse;
+import it.unical.ea.lemubackend.lemu_backend.config.security.TokenStore;
+import it.unical.ea.lemubackend.lemu_backend.data.entities.Utente;
 import it.unical.ea.lemubackend.lemu_backend.data.service.OrdineService;
 import it.unical.ea.lemubackend.lemu_backend.dto.OrdineDto;
-import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
-import jakarta.validation.Valid;
+import it.unical.ea.lemubackend.lemu_backend.dto.OrdineProdottoDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ordinecontroller-api")
@@ -22,15 +23,15 @@ public class OrdineController {
 
 
     @PostMapping("/add")
-    public ResponseEntity<OrdineDto> add(@RequestBody OrdineDto ordine, @RequestParam String jwt) {
-     return ResponseEntity.ok(ordineService.save(ordine, jwt));
-
+    public void add(@RequestBody OrdineDto ordineDto) {
+        ordineService.save(ordineDto);
     }
+
+
 
     @GetMapping("/all/{id}")
     public ResponseEntity<Collection<OrdineDto>> findbyUser(@PathVariable("id") Long id) {
-        Collection<OrdineDto> ordineDtos = ordineService.findOrderbyUser(id);
-        return (ordineDtos != null) ? ResponseEntity.ok(ordineDtos) : ResponseEntity.notFound().build();
+        return ResponseEntity.ok(ordineService.findOrderbyUser(id));
     }
 
     @GetMapping("/ordine/{orderId}")
@@ -39,7 +40,57 @@ public class OrdineController {
         return (ordine != null) ? ResponseEntity.ok(ordine) : ResponseEntity.notFound().build();    }
 
 
+    @GetMapping("/getAll")
+    public ResponseEntity<Collection<OrdineDto>> getAllOrders(){
+        return ResponseEntity.ok(ordineService.findAllOrders());
+    }
 
+    @GetMapping("/getDettagliOrdini{idOrder}")
+    public ResponseEntity<Collection<OrdineProdottoDto>> getDettagliOrdineByIdOrder(@PathVariable ("idOrder")Long id){
+        return ResponseEntity.ok(ordineService.getDettagliOrdineByIdOrdine(id));
+    }
+
+
+    /*
+    @GetMapping("/getOrdiniByidUtente{idUser}")
+    public ResponseEntity<Collection<OrdineDto>> getOrdinibyidUtente(HttpServletRequest request, @PathVariable ("idUser")Long id){
+        try {
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = TokenStore.getInstance().getToken(request);
+                Optional<Utente> optional = TokenStore.getInstance().getUser(token);
+                if(optional.isPresent()){
+                    return ResponseEntity.ok(ordineService.findOrderbyUser(optional.get().getId()));
+                }
+            } else {
+                //vanno gestiti gli errore
+            }
+        } catch (Exception e) {
+            return null;
+
+        }
+
+        return null;
+    }
+
+     */
+
+    @GetMapping("/getOrdiniByidUtente{idUser}")
+    public ResponseEntity<Collection<OrdineDto>> getOrdinibyidUtente(@PathVariable ("idUser")Long id){
+        return ResponseEntity.ok(ordineService.findOrderbyUser(id));
+    }
+
+
+    /*
+    @GetMapping("/ordini/{idUser}/filter")
+    public ResponseEntity<Collection<OrdineDto>> getOrdiniByUserAndDate(
+            @PathVariable("idUser") Long idUser,
+            @RequestParam("dateFilter") String dateFilter) {
+        Collection<OrdineDto> ordini = ordineService.findOrderByUserAndDate(idUser, dateFilter);
+        return ResponseEntity.ok(ordini);
+    }
+
+     */
 
 
 }
