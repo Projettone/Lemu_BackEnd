ee804f403c3cc909cac5e867a83682cb27fe4085
Cataldo Federico <fedex46@outlook.it>
2024-08-18 18:39:43 +0200

    backend: modifiche ordineDao e prodottoDao

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/OrdineDao.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/OrdineDao.java
@@ -1,9 +1,10 @@
 package it.unical.ea.lemubackend.lemu_backend.data.dao;
 
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Ordine;
-import it.unical.ea.lemubackend.lemu_backend.data.entities.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,6 @@ import java.util.List;
 @Repository
 public interface OrdineDao extends JpaRepository<Ordine, Long> {
 
-    List<Ordine> findOrdineByUtente_Id(Long id);
-
+    @Query("SELECT o FROM Ordine o WHERE o.utente.id = :utenteId")
+    List<Ordine> findOrdiniByUtenteId(@Param("utenteId") Long utenteId);
 }
--- src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/ProdottoDao.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/data/dao/ProdottoDao.java
@@ -2,8 +2,18 @@ package it.unical.ea.lemubackend.lemu_backend.data.dao;
 
 import it.unical.ea.lemubackend.lemu_backend.data.entities.Prodotto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProdottoDao extends JpaRepository<Prodotto, Long> {
+    @Query("SELECT p FROM Prodotto p WHERE p.nome LIKE %:keyword% OR p.descrizione LIKE %:keyword% OR p.categoria LIKE %:keyword%")
+    List<Prodotto> searchByKeyword(@Param("keyword") String keyword);
+
+
+    @Query("SELECT p FROM Prodotto p WHERE p.categoria = :categoria")
+    List<Prodotto> findByCategoria(@Param("categoria") String categoria);
+
 }
