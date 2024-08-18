121ee08237c5aba12229c2e3bd9e0a0cab9f7c34
Cataldo Federico <fedex46@outlook.it>
2024-08-18 18:41:01 +0200

    backend: modifiche ProdottoController

--- src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/ProdottoController.java
+++ src/main/java/it/unical/ea/lemubackend/lemu_backend/controller/ProdottoController.java
@@ -1,16 +1,12 @@
 package it.unical.ea.lemubackend.lemu_backend.controller;
 
-import com.nimbusds.jose.JOSEException;
-import it.unical.ea.lemubackend.lemu_backend.config.ApiResponse;
 import it.unical.ea.lemubackend.lemu_backend.data.service.ProdottoService;
 import it.unical.ea.lemubackend.lemu_backend.dto.ProdottoDto;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,20 +26,36 @@ public class ProdottoController {
 
  */
 
+
     @PostMapping("/add")
+    // @PreAuthorize("hasRole('ROLE_ADMIN')") //va aggiunto solo ai metodi che devino essere richiamati da admin
     public void add(@RequestBody ProdottoDto prodotto) {
         prodottoService.save(prodotto);
     }
 
     @GetMapping("/all")
-    public ResponseEntity<Collection<ProdottoDto>> findAll() {
-        return ResponseEntity.ok(prodottoService.findAll());
+    public ResponseEntity<Collection<ProdottoDto>> findAll(@RequestParam int start, @RequestParam int end) {
+        return ResponseEntity.ok(prodottoService.findAll(start, end));
     }
 
 
     @GetMapping("/get/{id}")
     public ResponseEntity<ProdottoDto> getById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(prodottoService.getById(id));
+    }
+
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProdottoDto>> searchProdotti(@RequestParam String keyword) {
+        return ResponseEntity.ok(prodottoService.searchProdotti(keyword));
+    }
+
+
+    //prendere tutti i prodotti di una determinata categoria
+
+    @GetMapping("/get/by-category/{categoria}")
+    public ResponseEntity<List<ProdottoDto>> getProdutCategory(@PathVariable("categoria") String categoria){
+        return ResponseEntity.ok(prodottoService.getProdottiByCategoria(categoria));
     }
 
 
